b95e45b156a6f21d6621820ae16d28df55f3d99a
zhouzhouthezhou <kylezhou2222@gmail.com>
2019-12-07 22:31:20 -0700

    fad

--- Python_code/IMUclass.py
+++ Python_code/IMUclass.py
@@ -58,7 +58,8 @@ class IMU:
 		self.writeTo(self.gyroSmpl, gyroRate)
 
 		#set gyro lowpass
-		gconfig1 = int.from_bytes(self.readFrom(self.gyroConfig1, 1), byteorder='big') & 0b10001110
+		test = self.readFrom(self.gyroConfig1, 1)
+		gconfig1 = int.from_bytes(test, byteorder='big') & 0b10001110
 		gconfig1 |= 0b1
 		gconfig1 |= (5 & 0x07) << 4
 		self.writeTo(self.gyroConfig1, gconfig1)
