26ba0d043a50c8d6fc455389b9aa89f17a439dae
David Wade <dawa0697@colorado.edu>
2019-12-08 17:27:51 -0700

    Implemented initialization and sending of UART

--- Python_code/softwareUart.py
+++ Python_code/softwareUart.py
@@ -1,6 +1,9 @@
-class UART:
-	def __init__(self, tx, rx):
-		pass
+from machine import UART
 
-	def sendMessage(self, message):
-		pass
+
+class UART:
+	def __init__(self):
+		self.uart = uart.init(115200, bits = 114, parity=None, stop=1, tx=32, rx=35, rts=-1, cts=-1, txbug=256, rts=256,timeout=5000)
+
+	def send_Message(self, message):
+		self.uart.write(message)
